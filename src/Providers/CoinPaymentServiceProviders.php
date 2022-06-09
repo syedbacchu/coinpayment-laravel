762a70a58a1ae17a34663b84ee0b6f1805c4f0a9
ubuntu-pc <bacchu.cse11@gmail.com>
2022-06-09 16:45:00 +0600

    initial commit

---
+++ src/Providers/CoinPaymentServiceProviders.php
@@ -0,0 +1,54 @@
+<?php
+
+
+namespace Sdtech\CoinpaymentsLaravel\Providers;
+
+
+use Illuminate\Support\ServiceProvider;
+
+class CoinPaymentServiceProviders extends ServiceProvider
+{
+
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
+
+    /**
+     * Bootstrap the application events.
+     *
+     * @param
+     */
+    public function boot()
+    {
+        $this->mergeConfigFrom(
+            __DIR__.'/../Config/coinpayments.php', 'coinpayments'
+        );
+        $this->publishFiles();
+    }
+
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+
+    /**
+     * Publish config file for the installer.
+     *
+     * @return void
+     */
+    protected function publishFiles()
+    {
+        $this->publishes([
+            __DIR__ . '/../Config/coinpayments.php' => config_path('coinpayments.php')]);
+    }
+
+}
